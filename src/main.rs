78a1cc1303d95d6b1348fcef901f4b39404460d4
Helder Martins <heldermartins89@gmail.com>
2018-10-22 23:05:14 +0100

    Delete temp file at end of program. Small refactor

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ use std::fs;
 use std::path::Path;
 use std::process;
 use std::process::Command;
-use std::process::Stdio;
-
 
 fn main() {
     let yaml = load_yaml!("../cmd.yml");
@@ -24,18 +22,20 @@ fn process_request(matches: ArgMatches) {
 
     // Having the file location we have to:
     // - Decrypt using the pgp command line tool
-    // - Retrieve the password from the user
     // - Read the file
     let file = decrypt_and_read_file(file_location).unwrap_or_else(|err| {
         eprintln!("Problem trying to read encrypted file: {}", err);
         process::exit(1);
     });
 
-    println!("file contents: {}\n", file.contents);
+    println!("file contents:\n {}\n", file.contents);
     // - process into friendly format
-    // - Read the required positions from user
-    // - Retrieve and print the values required.
+    // - Retrieve and print the values required based on the positions provided
     // - delete the decrypted file before exiting the application.
+    delete_temp_file(file.name.as_str()).unwrap_or_else(|err| {
+        eprintln!("Problem deleting the temporary file:\n{}\n", err);
+        process::exit(1);
+    });
 }
 
 struct File {
@@ -59,29 +59,40 @@ fn decrypt_and_read_file(file_location: &str) -> Result<File, &str> {
         return Err("Provided path is invalid");
     }
 
-    let mut process = match Command::new("gpg")
-                .arg("--output")
-                .arg(decrypted_file_name)
-                .arg("--decrypt")
-                .arg(file_path_str.unwrap())
-                .stdin(Stdio::piped())
-                .stdout(Stdio::piped())
-                .spawn() {
-        Err(reason) => panic!(reason),
-        Ok(process) => process,
-    };
-
-    process.wait().expect("Nothing is running...");
+    decrypt_file(decrypted_file_name, file_path_str.unwrap());
 
     Ok(File::new(decrypted_file_name.to_string(), read_file(decrypted_file_name)?))
 }
 
+fn decrypt_file(decrypted_file_name: &str, file_path_str: &str) {
+    let mut process =
+        match Command::new("gpg")
+            .arg("--output")
+            .arg(decrypted_file_name)
+            .arg("--decrypt")
+            .arg(file_path_str)
+            .spawn() {
+            Err(reason) => panic!(reason),
+            Ok(process) => process,
+        };
+
+    // Wait for the password input.
+    process.wait().expect("Nothing is running...");
+}
+
 fn read_file(decrypted_file_name: &str) -> Result<String, &str> {
     let fetch_result = fs::read_to_string(decrypted_file_name);
-    if let Err(_) = fetch_result {
+    if let Err(_) = fetch_result { // TODO pass propagate error message
         return Err("Unable to read decrypted file");
     }
 
     Ok(fetch_result.unwrap())
 }
 
+fn delete_temp_file(file: &str) -> Result<(), &str> {
+    if let Err(_) = fs::remove_file(file) {
+        return Err("Unable to delete temp file!!");
+    }
+
+    Ok(())
+}
